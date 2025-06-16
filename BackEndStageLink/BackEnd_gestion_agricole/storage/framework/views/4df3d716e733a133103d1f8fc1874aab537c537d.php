b698395e9e1798433d677125993df33ee13f0ffa
Byron-voldigoad <tchangolouismullergmail.com>
2025-06-16 18:18:58 +0100

    re:

--- BackEndStageLink/BackEnd_gestion_agricole/storage/framework/views/4df3d716e733a133103d1f8fc1874aab537c537d.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\stage\Soutenance Liscence\Projets TuTore\App-gestion-agricole\BackEnd_gestion_agricole\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
